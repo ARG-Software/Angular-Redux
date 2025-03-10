6ffa5b5e598fe26197b302cd8c1ba3a2c5fbe539
blekso <mihael.istvan@gmail.com>
2025-03-10 17:31:06 +0100

    fixed downtime component issue

--- src/app/features/data-warehouse/containers/machining/downtime/downtime.component.ts
+++ src/app/features/data-warehouse/containers/machining/downtime/downtime.component.ts
@@ -96,7 +96,10 @@ export class DowntimeComponent implements OnInit {
    * @param filters new selected filters
    */
   public filtersOptions(filters: any) {
-    this.request.Filters = filters;
+    this.request = {
+      ...this.request,
+      Filters: { ...filters },
+    };
     this.store.dispatch(new GetDowntimeData(this.request));
   }
 }
