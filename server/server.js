76bf81d11f5e1c774bd9254dbfeb307111a37ed2
blekso <mihael.istvan@gmail.com>
2025-03-20 17:09:12 +0100

    add DataWarehouse_graphics_DowntimePareto POST route

--- server/server.js
+++ server/server.js
@@ -12,6 +12,52 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
+server.use((req, res, next) => {
+  if (req.method === "POST" && !req.body.id) {
+    req.body.id = Date.now().toString();
+  }
+  next();
+});
+
+server.post("/DataWarehouse_graphics_DowntimePareto", (req, res) => {
+  const response = {
+    Success: true,
+    Result: {
+      ChartData: [
+        {
+          AssetNumber: "Machine-1",
+          DowntimeInMinutes: 234,
+          InstancesOfDowntime: 3,
+        },
+        {
+          AssetNumber: "Machine-2",
+          DowntimeInMinutes: 220,
+          InstancesOfDowntime: 8,
+        },
+      ],
+      TableData: {
+        Result: [
+          {
+            AssetNumber: "Machine-1",
+            DowntimeInMinutes: 189,
+            InstancesOfDowntime: 8,
+          },
+          {
+            AssetNumber: "Machine-2",
+            DowntimeInMinutes: 145,
+            InstancesOfDowntime: 8,
+          },
+        ],
+        Total: 2,
+      },
+    },
+    GeneratedAt: new Date().toISOString(),
+    Message: "Filtered data response.",
+  };
+
+  res.json(response);
+});
+
 router.render = (req, res) => {
   const body = res.locals.data;
 
